79f2859c847793fd74c8bb5707237203e4e2459e
igor-ivancha <sg.igor.iv@gmail.com>
2017-04-20 11:23:44 +0300

    parse query params

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-console.log(BrowserRouter);
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import './App.css'
 
-const App = () => <h1>Hello world!</h1>
+const Links = () => (
+  <nav>
+    <Link to="/?id=123">Inline</Link>
+    <Link to={{pathname: '/', search: 'id=456'}}>Object</Link>
+  </nav>
+)
+
+const App = (props) => (
+  <Router>
+    <div>
+      <Links />
+      <Route path="/" render={({match, location}) => (
+          <div>
+            <p>root</p>
+            <p>{JSON.stringify(match)}</p>
+            <p>{JSON.stringify(location)}</p>
+            <p>{new URLSearchParams(location.search).get('id')}</p>
+          </div>
+        )} />
+    </div>
+  </Router>
+)
 
 export default App
