9d5c3405860d8537855993ec73702fcee606c4ef
igor-ivancha <sg.igor.iv@gmail.com>
2017-04-20 11:47:14 +0300

    catch all routes with switch component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-console.log(BrowserRouter);
+import React, {Component} from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import './App.css'
 
-const App = () => <h1>Hello world!</h1>
+const Links = () =>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Links />
+          <Switch>
+            <Route exact path="/" render={() => <h1>Home</h1>} />
+            <Route path="/about" render={() => <h1>About</h1>} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
 
 export default App
